07748a00ee38654f791855c082b21ebdf8403faf
bitbonk <bitbonk@msn.com>
2024-08-28 08:49:32 +0200

    Setup project

---
+++ MyClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace MyClassLibrary1;
+
+public class Class1
+{
+    public void Method1(string? arg)
+    {
+        Console.WriteLine(arg);
+    }
+    
+    public void Method2(string? arg)
+    {
+        Console.WriteLine(arg);
+    }
+    
+    public void Method3(string? arg)
+    {
+        Console.WriteLine(arg);
+    }
+    
+    public void Method4(string? arg)
+    {
+        Console.WriteLine(arg);
+    }
+    
+    public void Method5(string? arg)
+    {
+        Console.WriteLine(arg);
+    }
+    
+    public void Method6(string? arg)
+    {
+        Console.WriteLine(arg);
+    }
+    
+    public void Method7(string? arg)
+    {
+        Console.WriteLine(arg);
+    }
+}
